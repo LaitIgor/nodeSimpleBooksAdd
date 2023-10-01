79e49dfc48bd39bc36853b4ff9fc31136bcb807a
Lait Igor <laitigorofficial@gmail.com>
2023-10-01 20:22:26 +0200

    add cookie and session dummy auth

--- app.js
+++ app.js
@@ -3,53 +3,72 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://dbAdmin:Y1q60anPExyCCjJ6@cluster0.0oej2p1.mongodb.net/shop';
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
-// middleware to parse query parameters from url
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
-    User.findById('651859f1804afab8fc078243')
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.error('err ', err));
-})
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-    .connect(
-        'mongodb+srv://dbAdmin:Y1q60anPExyCCjJ6@cluster0.0oej2p1.mongodb.net/shop?retryWrites=true&w=majority'
-        )
-    .then(() => {
-        User.findOne().then(user => {
-            if(!user) {
-                console.log('Should not come here since user exists!');
-                const user = new User({
-                    name: 'Igor',
-                    email: 'igor@email.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
-        })
-        app.listen(3000)
-    })
-    .catch(err => console.log('Error during connection through Mongoose', err));
+  .connect(MONGODB_URI)
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Igor',
+          email: 'igor@email.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- controllers/shop.js
+++ controllers/shop.js
@@ -4,123 +4,120 @@ const Order = require('../models/orders');
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then(products => {
-      console.log('Prods to render', products);
+      // console.log('products', products);
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
-        path: '/products'
+        path: '/products',
+        isAuthenticated: req.session.isLoggedIn
       });
     })
-    .catch(err => console.log('THere was an error while fetching products', err));
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
-    .then((product) => {
-      console.log('PRODUCT BY ID', product);
+    .then(product => {
       res.render('shop/product-detail', {
         product: product,
         pageTitle: product.title,
-        path: '/products'
+        path: '/products',
+        isAuthenticated: req.session.isLoggedIn
+      });
+    })
+    .catch(err => console.log('There was an error: ', err));
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.find()
+    .then(products => {
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        isAuthenticated: req.session.isLoggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.getCart = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .then(user => {
+      const products = user.cart.items;
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products,
+        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => console.log(err));
 };
 
-exports.getIndex = (req, res, next) => {
-  Product.find()
-    .then((products) => {
-      console.log('Prods to render in index ', products);
-      res.render('shop/index', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/'
-      });
-    })
-    .catch(err => console.log('errrrr', err));
-};
-
-exports.getCart = (req, res, next) => {
-  req.user.populate('cart.items.productId')
-    .then((user) => {
-      console.log('products111', user.cart.items);
-        const products = user.cart.items;
-        res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: products
-      });
-    })
-    .catch(err => console.log('THere is an error: ', err) )
-};
-
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
-    .then((product) => {
-      return req.user.addToCart(product)
+    .then(product => {
+      return req.user.addToCart(product);
     })
-    .then(() => {
+    .then(result => {
+      console.log(result);
       res.redirect('/cart');
-    })
-    .catch(err => console.log('THere is an error: ', err))
+    });
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  req.user.removeFromCart(prodId)
-    .then(() => {
+  req.user
+    .removeFromCart(prodId)
+    .then(result => {
       res.redirect('/cart');
     })
-    .catch(err => console.log('There is an error: ', err))
-};
-
-exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+    .catch(err => console.log(err));
 };
 
 exports.postOrder = (req, res, next) => {
-  req.user.populate('cart.items.productId')
-    .then((user) => {
-      console.log('products111', user.cart.items);
-        const products = user.cart.items.map(product => {
-          // ._doc retrieves all the data of a product like populate does 
-          // instead of just showin id object
-          return {quantity: product.quantity, product: {...product.productId._doc}}
-        });
-        const order = new Order({
-          user: {
-            name: req.user.name,
-            // Again, mongoose will pick id from whiole user object here
-            userId: req.user
-          },
-          products
-        });
-
-        return order.save();
+  req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      });
+      return order.save();
     })
-    .then(() => {
+    .then(result => {
       return req.user.clearCart();
     })
     .then(() => {
       res.redirect('/orders');
     })
-    .catch(err => console.log('THere is an error: ', err))
+    .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  Order.find({ "user.userId": req.user._id })
-    .then((orders) => {
+  Order.find({ 'user.userId': req.user._id })
+    .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
-        pageTitle: 'Your orders',
-        orders: orders
+        pageTitle: 'Your Orders',
+        orders: orders,
+        isAuthenticated: req.session.isLoggedIn
       });
     })
-    .catch(err => console.log('THere is an error: ', err))
-
+    .catch(err => console.log(err));
 };
