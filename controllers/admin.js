54c27bd6bf942c51a141688d754ad9360dd53249
Lait Igor <laitigorofficial@gmail.com>
2023-10-08 15:06:24 +0200

    finish add and edir products validation

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,19 @@
 const Product = require('../models/product');
+const { validationResult } = require('express-validator');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false,
-    isAuthenticated: req.isLoggedIn
+    hasError: false,
+    product: {
+      title: '',
+      imageUrl: '',
+      price: '',
+      description: ''
+    },
+    errorMessage: null,
   });
 };
 
@@ -14,6 +22,24 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const imageUrl = req.body.imageUrl;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log('errors.Array()', errors.array());
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      editing: false,
+      hasError: true,
+      product: {
+        title,
+        imageUrl,
+        price,
+        description
+      },
+      errorMessage: errors.array()[0].msg,
+    });
+  }
 
   const product = new Product(
     {
@@ -52,7 +78,9 @@ exports.getEditProduct = (req, res, next) => {
         path: '/admin/edit-product',
         editing: editMode,
         product: product,
-        isAuthenticated: req.isLoggedIn
+        hasError: false,
+        errorMessage: null,
+        validaTionErrors: [],
       });
     })
 };
@@ -63,12 +91,34 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: true,
+      hasError: true,
+      product: {
+        title: updatedTitle,
+        imageUrl: updatedImageUrl,
+        price: updatedPrice,
+        description: updatedDesc,
+        _id: prodId
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
+      
+    });
+  }
+
+  console.log('Does it ATLEAST PASS I|T?');
+  console.log('req.body: ', req.body);
+  console.log('FIND BY ID OF: ', prodId);
 
     Product.findById(prodId)
       .then(product => {
         // Ensure that only user who created product is deleting it
-        console.log(product.userId, 'product.userId');
-        console.log(req.user._id.toString(), 'req.user._id.toString()');
         if(product.userId.toString() !== req.user._id.toString()) {
           console.log('=======>> Forbidden Actions detected!!! <<=======');
           return res.redirect('/login');
@@ -83,7 +133,7 @@ exports.postEditProduct = (req, res, next) => {
             res.redirect('/admin/products')
           })
       })
-      .catch(err => console.warn('error', err))
+      .catch(err => console.warn('ERROR WHILE SEARCHING A PRODUCT', err))
 
   // const updatedProduct = new Product(
   //   prodId,
@@ -110,7 +160,6 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: 'Admin Products',
         path: '/admin/products',
-        isAuthenticated: req.isLoggedIn
 
       })
     })
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const express = require('express');
+const { body } = require('express-validator');
 
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/isAuth');
@@ -13,13 +14,46 @@ router.get('/add-product', isAuth, adminController.getAddProduct);
 
 // /admin/products => GET
 router.get('/products', isAuth, adminController.getProducts);
-
+// title
+// imageUrl
+// price
+// description
 // admin/add-product => POST
-router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post('/add-product',
+ [
+    body('title')
+    .trim()
+    .isString()
+    .isLength({min: 3}),
+    body('imageUrl')
+    .isURL(),
+    body('price')
+    .isFloat(),
+    body('description')
+    .trim()
+    .isLength({min: 5, max: 400}),
+ ],
+ isAuth, 
+ adminController.postAddProduct);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post('/edit-product',
+    [
+        body('title')
+        .trim()
+        .isString()
+        .isLength({min: 3}),
+        body('imageUrl')
+        .isURL(),
+        body('price')
+        .isFloat(),
+        body('description')
+        .trim()
+        .isLength({min: 5, max: 400}),
+    ],
+    isAuth, 
+    adminController.postEditProduct);
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
